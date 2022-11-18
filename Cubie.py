e1e939934afd5d7448964fe3fa29e3ea991ab57c
VelascoAMath <alfredo0234@gmail.com>
2022-11-17 21:33:08 -0500

    Added a render option so that we can visualize the cubie with vpython. Also, we have comments on the methods

--- Cubie.py
+++ Cubie.py
@@ -1,28 +1,59 @@
 from dataclasses import dataclass
 import numpy as np
 
+from vpython import *
 
 
+color_to_vec = {
+	"r": vector(1, 0, 0),
+	"w": vector(1, 1, 1),
+	"b": vector(0, 0, 1),
+	"g": vector(0, 1, 0),
+	"o": vector(1, 0.5, 0),
+	"y": vector(1, 1, 0),
+}
 
+
+def np_to_vector(x):
+	return vector(x[0][0], x[1][0], x[2][0] )
 
 @dataclass
 class Cubie(object):
+	'''
+	This class represents an individual cubie that's found on a Rubik's cube
+	'''
 
+	# x,y,z position of the cubie
 	pos         : np.array = None
+	# Dictionary that maps each color to the norm of the surface on the face
 	color_to_dir: dict     = None
 
 
 
 	def set_position(self, x, y, z):
+		'''
+		Set the position of the cubie
+		param x
+		param y
+		param z
+		'''
 		self.pos = np.array([[x], [y], [z]])
 		self.color_to_dir = dict()
 
 	def set_color(self, color, x, y, z):
+		'''
+		Adds a color with the norm of the face with that color
+		param color - the color. Should be a string in 'rwbgoy'
+		'''
 		self.color_to_dir[color] = np.array([[x], [y], [z]])
 
 
 	def rotate(self, axis, angle):
 
+		'''
+		param: axis - The axis of rotation
+		param: angle - The amount (degrees) to rotate the cubie around the origin
+		'''
 		angle = np.radians(angle)
 
 		if axis == 'x':
@@ -38,6 +69,32 @@ class Cubie(object):
 		for color, v  in self.color_to_dir.items():
 			self.color_to_dir[color] = np.round(R @ v)
 
+	def render(self):
+		'''
+		Render the cubie using vpython
+		'''
+		for color, v in self.color_to_dir.items():
+			# Face points forward/back
+			if v[0][0] != 0:
+				p1 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0], [0.5], [0.5]]) ), color=color_to_vec[color])
+				p2 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0], [0.5], [-0.5]]) ), color=color_to_vec[color])
+				p3 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0], [-0.5], [0.5]]) ), color=color_to_vec[color])
+				p4 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0], [-0.5], [-0.5]]) ), color=color_to_vec[color])
+				Q = quad(vs=[p1,p2,p3,p4])
+			# Face points up/down
+			elif v[1][0] != 0:
+				p1 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0.5], [0], [0.5]]) ), color=color_to_vec[color])
+				p2 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0.5], [0], [-0.5]]) ), color=color_to_vec[color])
+				p3 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[-0.5], [0], [0.5]]) ), color=color_to_vec[color])
+				p4 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[-0.5], [0], [-0.5]]) ), color=color_to_vec[color])
+				Q = quad(vs=[p1,p2,p3,p4])
+			# Face points left/right
+			elif v[2][0] != 0:
+				p1 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0.5], [0.5], [0]]) ), color=color_to_vec[color])
+				p2 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[0.5], [-0.5], [0]]) ), color=color_to_vec[color])
+				p3 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[-0.5], [0.5], [0]]) ), color=color_to_vec[color])
+				p4 = vertex(pos=np_to_vector(self.pos + v / 2 + np.array([[-0.5], [-0.5], [0]]) ), color=color_to_vec[color])
+				Q = quad(vs=[p1,p2,p3,p4])
 
 
 
@@ -49,8 +106,9 @@ def main():
 	c.set_color('r', 0, 1, 0)
 	c.set_color('w', 0, 0, 1)
 	c.set_color('b', 1, 0, 0)
-	c.rotate("z", 90)
+	c.rotate("z", 180)
 	print(c)
+	c.render()
 
 
 if __name__ == '__main__':
