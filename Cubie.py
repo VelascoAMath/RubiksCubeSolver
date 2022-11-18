3148c52261971a9b5dbaa0fe0ba8e0d9005380d8
VelascoAMath <alfredo0234@gmail.com>
2022-11-17 20:14:01 -0500

    Added a new vector that maps the colors of the sides of the cubie to where it's looking. We also added a method to add the colors and the rotate method will also rotate the colors

--- Cubie.py
+++ Cubie.py
@@ -8,11 +8,18 @@ import numpy as np
 @dataclass
 class Cubie(object):
 
-	pos:np.array = None
+	pos         : np.array = None
+	color_to_dir: dict     = None
+
 
 
 	def set_position(self, x, y, z):
 		self.pos = np.array([[x], [y], [z]])
+		self.color_to_dir = dict()
+
+	def set_color(self, color, x, y, z):
+		self.color_to_dir[color] = np.array([[x], [y], [z]])
+
 
 	def rotate(self, axis, angle):
 
@@ -28,6 +35,8 @@ class Cubie(object):
 			raise Exception(f"{axis} is not a valid axis! Must be x, y, or z!")
 
 		self.pos = np.round(R @ self.pos)
+		for color, v  in self.color_to_dir.items():
+			self.color_to_dir[color] = np.round(R @ v)
 
 
 
@@ -37,6 +46,9 @@ def main():
 	c = Cubie()
 
 	c.set_position(1, 0, 0)
+	c.set_color('r', 0, 1, 0)
+	c.set_color('w', 0, 0, 1)
+	c.set_color('b', 1, 0, 0)
 	c.rotate("z", 90)
 	print(c)
 
